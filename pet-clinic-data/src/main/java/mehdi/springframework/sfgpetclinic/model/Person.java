063d7ab208c19195f4d07a2edd3a7029bbede4cc
Mehdibhy0 <benhadjyahiamehdi@gmail.com>
2022-02-16 15:27:32 +0100

    adding Interfaces for services , closes #5.

--- pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/model/Person.java
@@ -6,18 +6,11 @@ public class Person {
     private String lastName;
 
     public String getFirstName() {
-        return firstName;
-    }
+        return firstName;}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setFirstName(String firstName) {this.firstName = firstName;}
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getLastName() {return lastName;}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName(String lastName) {this.lastName = lastName;}
 }
---
+++ pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,16 @@
+package mehdi.springframework.sfgpetclinic.services;
+
+
+import mehdi.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
