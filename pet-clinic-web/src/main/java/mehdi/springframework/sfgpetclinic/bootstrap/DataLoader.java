2a9f36421c9c24c7155677a2a536c07fd302deb5
Mehdibhy0 <benhadjyahiamehdi@gmail.com>
2022-02-21 13:08:03 +0100

    //List vets on vet Index  , closes #20.

--- pet-clinic-web/src/main/java/mehdi/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mehdi/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -52,8 +52,8 @@ public class DataLoader implements CommandLineRunner {
 
         Vet vet2=new Vet();
         vet2.setId(2L);
-        vet2.setFirstName("Sam");
-        vet2.setLastName("Axe");
+        vet2.setFirstName("jason");
+        vet2.setLastName("derulo");
 
         vetService.save(vet2);
 
