65bce0dd0ebe39317bc150b3b195120ce5535d43
Mehdibhy0 <benhadjyahiamehdi@gmail.com>
2022-02-21 11:55:46 +0100

    Added Index page and Controller of owners , closes #18.

--- pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,12 +1,11 @@
 package mehdi.springframework.sfgpetclinic.services.map;
 
-import mehdi.springframework.sfgpetclinic.model.Owner;
 import mehdi.springframework.sfgpetclinic.model.Pet;
-import mehdi.springframework.sfgpetclinic.services.CrudService;
+import mehdi.springframework.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
