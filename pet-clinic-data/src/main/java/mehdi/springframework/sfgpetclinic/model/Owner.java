b2e69c747fed8367303ed8c24f07c5d6afa4f7b9
Mehdibhy0 <benhadjyahiamehdi@gmail.com>
2022-03-07 10:54:53 +0100

    Create Pet Type,Pet and Visit Entities closes #35

--- pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package mehdi.springframework.sfgpetclinic.model;
 
+import java.util.*;
+
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
