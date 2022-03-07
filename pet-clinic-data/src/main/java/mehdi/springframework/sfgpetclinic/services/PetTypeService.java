7f38b8590ea235028623808790b6ca2cc2a95497
Mehdibhy0 <benhadjyahiamehdi@gmail.com>
2022-03-07 11:37:56 +0100

    Create PetType Service ,closes #38

---
+++ pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package mehdi.springframework.sfgpetclinic.services;
+
+import mehdi.springframework.sfgpetclinic.model.*;
+
+public interface PetTypeService extends  CrudService<PetType,Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package mehdi.springframework.sfgpetclinic.services.map;
+
+import mehdi.springframework.sfgpetclinic.model.*;
+import mehdi.springframework.sfgpetclinic.services.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
