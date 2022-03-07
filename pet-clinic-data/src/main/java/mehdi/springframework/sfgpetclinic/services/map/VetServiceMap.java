d5ec97b699522d76df129d31c7859072e916d18b
Mehdibhy0 <benhadjyahiamehdi@gmail.com>
2022-03-07 14:35:38 +0100

    Create Speciality Service closes #40

--- pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/mehdi/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,15 +1,18 @@
 package mehdi.springframework.sfgpetclinic.services.map;
+
 import mehdi.springframework.sfgpetclinic.model.Vet;
 import mehdi.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
+
 import java.util.Set;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
     }
+
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
